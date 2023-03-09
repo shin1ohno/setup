c8ea8bb6b8d00f380ab87cf49b2ab98c5b4702dd
Shin'ichi Ohno <ohno@cookpad.com>
2023-03-09 11:44:10 +0000

    brew tap homebrew/cask-fonts before install fonts

--- cookbooks/fonts/default.rb
+++ cookbooks/fonts/default.rb
@@ -1,10 +1,10 @@
 case node[:platform]
 when "darwin"
-  execute 'brew reinstall --cask font-hack-nerd-font' do
+  execute 'brew tap homebrew/cask-fonts && brew reinstall --cask font-hack-nerd-font' do
     not_if %q{brew list | fgrep -q 'font-hack-nerd-font'}
   end
 
-  execute 'brew reinstall --cask font-sauce-code-pro-nerd-font' do
+  execute 'brew tap homebrew/cask-fonts && brew reinstall --cask font-sauce-code-pro-nerd-font' do
     not_if %q{brew list | fgrep -q 'font-sauce-code-pro-nerd-font'}
   end
 end
