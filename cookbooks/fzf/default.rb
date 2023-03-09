7dd5e387720936247ffb605b2a7e2bd3660c30b5
Shin'ichi Ohno <ohno@cookpad.com>
2023-03-09 23:46:02 +0000

    nord colors

--- cookbooks/fzf/default.rb
+++ cookbooks/fzf/default.rb
@@ -10,5 +10,9 @@ source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
 export FZF_COMPLETION_TRIGGER='``'
 export FZF_DEFAULT_COMMAND='fd --type file'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+--color fg:#D8DEE9,bg:#2E3440,hl:#A3BE8C,fg+:#D8DEE9,bg+:#434C5E,hl+:#A3BE8C
+--color pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B
+'
 EOM
 end
