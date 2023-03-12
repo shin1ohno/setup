9d26eea077a833fbb4b0721d8f73207bc228d26e
Shin'ichi Ohno <ohno@cookpad.com>
2023-03-12 23:11:44 +0000

    git config

--- cookbooks/git/default.rb
+++ cookbooks/git/default.rb
@@ -2,3 +2,10 @@
 
 package "git"
 package "git-lfs"
+
+remote_file "#{ENV["HOME"]}/.gitconfig" do
+  owner node[:setup][:user]
+  group node[:setup][:group]
+  mode "755"
+  source "files/gitconfig"
+end
