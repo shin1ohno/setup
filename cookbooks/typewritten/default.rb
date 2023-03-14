80397fb6614034ca73f20f2b948f3d8e4e39a5aa
Shin'ichi Ohno <ohno@cookpad.com>
2023-03-14 22:25:44 +0000

    use git clone to install typewritten on ubuntu

--- cookbooks/typewritten/default.rb
+++ cookbooks/typewritten/default.rb
@@ -1,15 +1,38 @@
 # frozen_string_literal: true
 
-package "typewritten"
+case node[:platform]
+when "darwin"
+  then
+    package "typewritten"
 
-add_profile "homebrew" do
-  bash_content <<"EOM"
-ZSH_THEME=""
-TYPEWRITTEN_PROMPT_LAYOUT="multiline"
-TYPEWRITTEN_RELATIVE_PATH="adaptive"
-TYPEWRITTEN_CURSOR="terminal"
-TYPEWRITTEN_DISABLE_RETURN_CODE=true
-autoload -U promptinit; promptinit
-prompt typewritten
+    add_profile "typewritten" do
+      bash_content <<"EOM"
+      ZSH_THEME=""
+      TYPEWRITTEN_PROMPT_LAYOUT="multiline"
+      TYPEWRITTEN_RELATIVE_PATH="adaptive"
+      TYPEWRITTEN_CURSOR="terminal"
+      TYPEWRITTEN_DISABLE_RETURN_CODE=true
+      autoload -U promptinit; promptinit
+      prompt typewritten
 EOM
+    end
+else
+  execute "git clone https://github.com/reobin/typewritten.git #{node[:setup][:root]}/typewritten" do
+    not_if { File.exists? "#{node[:setup][:root]}/typewritten" }
+  end
+  
+   add_profile "typewritten" do
+      bash_content <<"EOM"
+      fpath+=#{node[:setup][:root]}/typewritten
+      autoload -U promptinit; promptinit
+      prompt typewritten
+      ZSH_THEME=""
+      TYPEWRITTEN_PROMPT_LAYOUT="multiline"
+      TYPEWRITTEN_RELATIVE_PATH="adaptive"
+      TYPEWRITTEN_CURSOR="terminal"
+      TYPEWRITTEN_DISABLE_RETURN_CODE=true
+      autoload -U promptinit; promptinit
+      prompt typewritten
+EOM
+    end
 end
