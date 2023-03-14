fc743082fed60fc0a8ceaac82f9daed23807b499
Shin'ichi Ohno <ohno@cookpad.com>
2023-03-14 22:23:35 +0000

    skip envchain on ubuntu

--- cookbooks/envchain/default.rb
+++ cookbooks/envchain/default.rb
@@ -7,8 +7,7 @@ when "arch"
   include_cookbook "arch-wanko-cc"
   package "envchain"
 when "ubuntu"
-  include_cookbook "apt-source-cookpad"
-  package "envchain"
+  #skip!
 else
   raise NotImplementedError
 end
