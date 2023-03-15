32922e3554a5c9417ecf14eba0b61f985b7e27fc
Shin'ichi Ohno <ohno@cookpad.com>
2023-03-15 00:06:56 +0000

    fix fzf on ubuntu

--- cookbooks/fzf/default.rb
+++ cookbooks/fzf/default.rb
@@ -2,8 +2,10 @@
 
 package "fzf"
 
+case node[:platform]
+when "darwin"
 add_profile "fzf" do
-  bash_content <<"EOM"
+  bash_content <<-EOM
 if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
   PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
 fi
@@ -17,4 +19,15 @@ export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
 --color pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B
 '
 EOM
+end
+
+when "ubuntu"
+add_profile "fzf" do
+  bash_content <<-EOM
+[[ $- == *i* ]] && source "/usr/share/doc/fzf/examples/key-bindings.zsh" 2> /dev/null
+--color fg:#D8DEE9,bg:#2E3440,hl:#A3BE8C,fg+:#D8DEE9,bg+:#434C5E,hl+:#A3BE8C
+--color pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B
+'
+EOM
+end
 end
