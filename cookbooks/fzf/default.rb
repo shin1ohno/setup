0360213fa8d2d2d9d6ff5dd740af794fea446579
Shin'ichi Ohno <ohno@cookpad.com>
2023-03-15 00:16:53 +0000

    fix fzf

--- cookbooks/fzf/default.rb
+++ cookbooks/fzf/default.rb
@@ -25,9 +25,6 @@ when "ubuntu"
 add_profile "fzf" do
   bash_content <<-EOM
 [[ $- == *i* ]] && source "/usr/share/doc/fzf/examples/key-bindings.zsh" 2> /dev/null
---color fg:#D8DEE9,bg:#2E3440,hl:#A3BE8C,fg+:#D8DEE9,bg+:#434C5E,hl+:#A3BE8C
---color pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B
-'
 EOM
 end
 end
