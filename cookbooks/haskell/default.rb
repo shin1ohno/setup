79db434091ac43a6a4c24ec2b41e08a684b4f332
Shin'ichi Ohno <ohno@cookpad.com>
2023-05-12 22:31:42 +0100

    Set ghcup path

--- cookbooks/haskell/default.rb
+++ cookbooks/haskell/default.rb
@@ -11,5 +11,11 @@ else
   execute "curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | BOOTSTRAP_HASKELL_NONINTERACTIVE=1 BOOTSTRAP_HASKELL_GHC_VERSION=latest BOOTSTRAP_HASKELL_CABAL_VERSION=latest BOOTSTRAP_HASKELL_INSTALL_STACK=0 BOOTSTRAP_HASKELL_INSTALL_HLS=1 BOOTSTRAP_HASKELL_ADJUST_BASHRC=P sh" do
     not_if "which ghcup"
   end
+  
+add_profile "pm2" do
+  bash_content <<'EOM'
+    export PATH="~/.ghcup/bin:$PATH"
+EOM
+end
 end
 
