07fecd77651e4c931c8ee00476bf298597b6fec1
Shin'ichi Ohno <ohno@cookpad.com>
2023-10-12 13:51:07 +0100

    Don't install neovim when it's there even not from snap

--- cookbooks/neovim/default.rb
+++ cookbooks/neovim/default.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 if node[:platform] == "ubuntu"
-  execute "snap install nvim --edge --classic && apt-get install python3-pynvim && apt-get remove neovim" do
-    not_if "snap list | grep -q neovim"
+  execute "snap install nvim --edge --classic && apt-get install python3-pynvim" do
+    not_if "which nvim"
   end
 else
   package "neovim"
