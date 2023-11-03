3924725b823a2fb4927587ea72217e5ff8114b2d
Shin'ichi Ohno <ohno@cookpad.com>
2023-11-03 17:20:17 +0000

    Install Lazygit to Ubuntu

--- cookbooks/git/default.rb
+++ cookbooks/git/default.rb
@@ -16,6 +16,19 @@ else
     user "root"
   end
 end
+
+if node[:platform] == "ubuntu"
+  execute "Install Lazygit" do
+    code <<-EOF
+    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+    EOF
+    user root
+  end
+end
+
 remote_file "#{ENV["HOME"]}/.gitconfig" do
   owner node[:setup][:user]
   group node[:setup][:group]
