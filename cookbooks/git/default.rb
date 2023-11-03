4dc5828bb65551559dc854593dd0e4b06b1924ce
Ubuntu <ubuntu@ip-192-168-0-139.eu-west-2.compute.internal>
2023-11-03 18:44:01 +0000

    fix syntax error

--- cookbooks/git/default.rb
+++ cookbooks/git/default.rb
@@ -19,13 +19,13 @@ end
 
 if node[:platform] == "ubuntu"
   execute "Install Lazygit" do
-    code <<-EOF
+    command <<-EOF
     LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
     EOF
-    user root
+    user "root"
   end
 end
 
