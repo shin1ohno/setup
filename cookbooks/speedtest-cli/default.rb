a33d26f0a8276cccaa6863c72a412b126f265770
Shin'ichi Ohno <shin1ohno@gmail.com>
2024-05-11 15:12:17 +0000

    speedtest-cli

---
+++ cookbooks/speedtest-cli/default.rb
@@ -0,0 +1,29 @@
+case node[:platform]
+when "darwin"
+  execute "brew tap teamookla/speedtest && brew update" do
+    not_if "which speedtest"
+  end
+when "ubuntu" 
+  directory "#{node[:setup][:root]}/speedtest-cli" do
+    owner node[:setup][:user]
+    group node[:setup][:group]
+    mode "755"
+  end
+
+  remote_file "#{node[:setup][:root]}/speedtest-cli/script.deb.sh" do
+    owner node[:setup][:user]
+    group node[:setup][:group]
+    mode "755"
+    source "files/script.deb.sh"
+  end
+
+  execute "Install speedtest-cli" do
+    command "#{node[:setup][:root]}/speedtest-cli/script.deb.sh"
+    action :run
+    not_if "which speedtest"
+  end
+end
+
+package "speedtest" do
+  not_if "which speedtest"
+end
