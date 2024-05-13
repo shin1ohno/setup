c6fd7bc98da6e65194c67fafb39b9fd2e09faaac
Shin'ichi Ohno <shin1ohno@gmail.com>
2024-05-13 20:02:46 +0900

    Go use the latest by default in profile

--- cookbooks/golang/default.rb
+++ cookbooks/golang/default.rb
@@ -21,15 +21,16 @@ execute "GVM_NO_UPDATE_PROFILE=1 #{node[:setup][:root]}/gvm-install.sh" do
   not_if "test -d #{ENV['HOME']}/.gvm"
 end
 
+go_versions = node[:go][:versions]
+go_default_version = go_versions[0]
+
 add_profile "gvm" do
   priority 60 # Make sure this is loaded after nodebrew
   bash_content <<-BASH
   source $HOME/.gvm/scripts/gvm
+  gvm use #{go_default_version}
   BASH
 end
-
-go_versions = node[:go][:versions]
-go_default_version = go_versions[0]
 
 go_versions.each do |v|
   execute "install Go version: #{v}" do
