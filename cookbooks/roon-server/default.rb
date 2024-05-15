6578befc508348f9499b336b95c71de3a83c0e69
Shin'ichi Ohno <shin1ohno@gmail.com>
2024-05-15 21:09:56 +0900

    RoonServer for linux

--- cookbooks/roon-server/default.rb
+++ cookbooks/roon-server/default.rb
@@ -2,7 +2,7 @@
 
 case node[:platform]
 when "darwin"
- remote_file "#{node[:setup][:root]}/roon/com.roon.server.plist" do
+  remote_file "#{node[:setup][:root]}/roon/com.roon.server.plist" do
     owner node[:setup][:user]
     group node[:setup][:group]
     mode "755"
@@ -11,4 +11,28 @@ when "darwin"
 
   execute "sudo cp #{node[:setup][:root]}/roon/com.roon.server.plist /Library/LaunchDaemons/com.roon.server.plist"
   execute "sudo chown root:wheel /Library/LaunchDaemons/com.roon.server.plist" 
+when "ubuntu"
+  %w(curl ffmpeg cifs-utils).each do |pkg| 
+    package pkg
+  end
+ 
+  directory "#{node[:setup][:root]}/roon" do
+    owner node[:setup][:user]
+    group node[:setup][:group]
+    mode "755"
+  end
+  
+  script_path = "#{node[:setup][:root]}/roon-server/linuxx64.sh" 
+
+  remote_file script_path do
+    owner node[:setup][:user]
+    group node[:setup][:group]
+    mode "755"
+    source "files/roonserver-installer-linuxx64.sh"
+  end
+
+  execute script_path do
+    user node[:setup][:user]
+    not_if "test -e /opt/RoonServer"
+  end
 end
