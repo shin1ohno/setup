fcc9deb16defccec4c7fab9f0633c136979e643f
Shin'ichi Ohno <shin1ohno@gmail.com>
2024-06-22 16:08:32 +0900

    Speedtest cli python version for Ubuntu

--- cookbooks/speedtest-cli/default.rb
+++ cookbooks/speedtest-cli/default.rb
@@ -3,27 +3,13 @@ when "darwin"
   execute "brew tap teamookla/speedtest && brew update" do
     not_if "which speedtest"
   end
+
+  package "speedtest"
 when "ubuntu" 
-  directory "#{node[:setup][:root]}/speedtest-cli" do
-    owner node[:setup][:user]
-    group node[:setup][:group]
-    mode "755"
-  end
-
-  remote_file "#{node[:setup][:root]}/speedtest-cli/script.deb.sh" do
-    owner node[:setup][:user]
-    group node[:setup][:group]
-    mode "755"
-    source "files/script.deb.sh"
-  end
-
   execute "Install speedtest-cli" do
-    command "#{node[:setup][:root]}/speedtest-cli/script.deb.sh"
-    action :run
+    command "pip install speedtest-cli"
     not_if "which speedtest"
   end
 end
 
-package "speedtest-cli" do
-  not_if "which speedtest"
-end
+
