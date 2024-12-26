3a77562ef339f075efaaf2c701f02499a22bab09
Shin'ichi Ohno <shin1ohno@gmail.com>
2024-12-26 20:24:51 +0900

    Tailscale

---
+++ cookbooks/tailscale/default.rb
@@ -0,0 +1,11 @@
+remote_file "#{node[:setup][:root]}/tailscale.sh" do
+  owner node[:setup][:user]
+  group node[:setup][:group]
+  mode "755"
+  source "files/install.sh"
+end
+
+execute "#{node[:setup][:root]}/tailscale.sh" do
+  not_if "test -f /usr/bin/tailscale"
+end
+
