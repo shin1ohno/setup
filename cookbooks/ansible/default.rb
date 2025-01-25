613e412e47de621c770d5dd6b079b58355de550d
Shin'ichi Ohno <shin1ohno@gmail.com>
2025-01-26 03:21:47 +0900

    Ansible for Yamaha network config

--- cookbooks/ansible/default.rb
+++ cookbooks/ansible/default.rb
@@ -1,4 +1,5 @@
-package "ansible" do
-  user "root"
+execute "install ansible" do
+  command "pip install ansible || ansible-galaxy collection install ansible.netcommon"
+  not_if "pip list | grep ansible"
 end
 
--- cookbooks/yamaha-network/default.rb
+++ cookbooks/yamaha-network/default.rb
@@ -1,3 +1,8 @@
+execute "install ansible-pylibssh" do
+  command "pip install ansible-pylibssh"
+  not_if "pip list | grep ansible-pylibssh"
+end
+
 execute "install rtx router ansible module" do
   command "ansible-galaxy collection install yamaha_network.rtx"
   not_if "ansible-galaxy collection list | grep yamaha_network.rtx"
