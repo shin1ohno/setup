ebbcfc224eaeab143e3467547d295f026a803de1
Shin'ichi Ohno <shin1ohno@gmail.com>
2025-02-02 02:25:43 +0900

    Zoxide

---
+++ cookbooks/zoxide/default.rb
@@ -0,0 +1,19 @@
+remote_file "#{node[:setup][:root]}/zoxide-install.sh" do
+  owner node[:setup][:user]
+  group node[:setup][:group]
+  mode "755"
+  source "files/install.sh"
+end
+
+# Hit ENTER automatically when asked to install CLI tools.
+# HAVE_SUDO_ACCESS=0 is required to skip `sudo` capability check.
+execute "echo | env #{node[:setup][:root]}/zoxide-install.sh" do
+  not_if "which z"
+end
+
+add_profile "zoxide" do
+  bash_content <<-BASH
+  eval "$(zoxide init zsh)"
+  BASH
+end
+
