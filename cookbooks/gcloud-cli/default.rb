8a795f06c1d594cc13e705411e8158f9a4e892b6
Shin'ichi Ohno <shin1ohno@gmail.com>
2025-04-17 01:34:20 +0900

    fix: Change installation path of gcloud-cli

--- cookbooks/gcloud-cli/default.rb
+++ cookbooks/gcloud-cli/default.rb
@@ -1,54 +1,41 @@
 # frozen_string_literal: true
 
-directory "#{node[:setup][:root]}/gcloud-cli" do
+installation_home = "#{node[:setup][:root]}/gcloud-cli"
+
+directory installation_home do
   owner node[:setup][:user]
-  group node[:setup][:group]
   mode "755"
 end
 
 # Get user information from node attributes
 user = node[:setup][:user]
-group = node[:setup][:group]
-home = node[:setup][:user_home] || ENV["HOME"]
-sdk_dir = "#{home}/google-cloud-sdk"
+sdk_dir = "#{installation_home}/google-cloud-sdk"
 gcloud_bin = "#{sdk_dir}/bin/gcloud"
-zshrc_path = "#{home}/.zshrc"
+zshrc_path = "#{installation_home}/.zshrc"
 path_line = "source '#{sdk_dir}/path.zsh.inc'"
 completion_line = "source '#{sdk_dir}/completion.zsh.inc'"
 
 case node[:platform]
 when "darwin"
-  # Detect architecture and set appropriate archive
-  arch_cmd = "uname -m"
-  arch = `#{arch_cmd}`.strip
-  
-  if arch == "arm64"
-    archive_name = "google-cloud-cli-darwin-arm.tar.gz"
-  else
-    archive_name = "google-cloud-cli-darwin-x86_64.tar.gz"
-  end
-  
+  archive_name = "google-cloud-cli-darwin-arm.tar.gz"
   archive_path = "#{node[:setup][:root]}/gcloud-cli/#{archive_name}"
   
   execute "curl --silent --fail https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/#{archive_name} -o #{archive_path.shellescape}" do
     user user
-    group group
     not_if { FileTest.exist?(archive_path) }
   end
   
-  execute "tar -xzf #{archive_path.shellescape} -C #{home}" do
+  execute "tar -xzf #{archive_path.shellescape} -C #{installation_home}" do
     user user
-    group group
     not_if { FileTest.directory?(sdk_dir) }
   end
   
   execute "#{sdk_dir}/install.sh --quiet --usage-reporting=false --path-update=true --command-completion=true --rc-path=#{zshrc_path} --additional-components alpha beta" do
     user user
-    group group
     not_if "test -x #{gcloud_bin}"
   end
 
-  profile "gcloud-cli" do
+  add_profile "gcloud-cli" do
     bash_content <<~EOM
       #{path_line}
       #{completion_line}
@@ -59,23 +46,20 @@ when "ubuntu", "debian"
   
   execute "curl --silent --fail https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-linux-x86_64.tar.gz -o #{archive_path.shellescape}" do
     user user
-    group group
     not_if { FileTest.exist?(archive_path) }
   end
   
-  execute "tar -xzf #{archive_path.shellescape} -C #{home}" do
+  execute "tar -xzf #{archive_path.shellescape} -C #{installation_home}" do
     user user
-    group group
     not_if { FileTest.directory?(sdk_dir) }
   end
   
   execute "#{sdk_dir}/install.sh --quiet --usage-reporting=false --path-update=true --command-completion=true --rc-path=#{zshrc_path} --additional-components alpha beta" do
     user user
-    group group
     not_if "test -x #{gcloud_bin}"
   end
 
-  profile "gcloud-cli" do
+  add_profile "gcloud-cli" do
     bash_content <<~EOM
       #{path_line}
       #{completion_line}
