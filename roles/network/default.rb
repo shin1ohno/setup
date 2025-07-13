a266e1e941d2fdb72f7c5134a4cbdc3dab092c3e
Shin'ichi Ohno <shin1ohno@gmail.com>
2025-07-14 00:11:09 +0900

    network: remove automation cookbooks
    Remove ansible and yamaha-network from network role to focus on
    core connectivity and testing tools

--- roles/network/default.rb
+++ roles/network/default.rb
@@ -1,10 +1,6 @@
 # Network connectivity and VPN
 include_cookbook "tailscale"
 
-# Network automation and management
-include_cookbook "ansible"
-include_cookbook "yamaha-network"
-
 # Network testing and monitoring tools
 include_cookbook "speedtest-cli"
 include_cookbook "iperf3"
