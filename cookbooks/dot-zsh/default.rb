a7592bb14d6e4c937791c288decdf98544f282f6
Shin'ichi Ohno <shin1ohno@gmail.com>
2026-01-18 22:06:44 +0900

    dot-zsh: add setup root bin directory to PATH
    Enable executables placed in ~/.setup_shin1ohno/bin to be
    accessible from the shell without full path specification.

--- cookbooks/dot-zsh/default.rb
+++ cookbooks/dot-zsh/default.rb
@@ -6,7 +6,7 @@ include_cookbook "oh-my-zsh"
 add_profile "dot-zsh" do
   priority 10
   bash_content <<"EOM"
-export PATH=#{ENV["HOME"]}/.local/bin:$PATH
+export PATH=#{node[:setup][:root]}/bin:#{ENV["HOME"]}/.local/bin:$PATH
 export ARCHPREFERENCE=arm64
 alias bri="envchain bricolage bundle exec bricolage"
 autoload -U bashcompinit
