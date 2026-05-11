fb52ea16a445b8dfbc92290a352c27d7f30d9733
Ohno Shin'ichi <shin1ohno@gmail.com>
2026-05-11 23:40:32 +0900

    Revert: aws login --remote bootstrap experiment (#339, #340) (#343)
    * Revert "aws-credentials: write login_session at compile time + use require_external_auth (#340)"
    
    This reverts commit dd8341028b22c134e99850788c785a59be046ffc.
    
    * Revert "functions: require_external_auth bootstraps aws CLI + aws login --remote (#339)"
    
    This reverts commit 2cb859f3808e262fd7428efd63ad238593214e19.

--- pve/lxc-cognee.rb
+++ pve/lxc-cognee.rb
@@ -24,26 +24,6 @@ node.reverse_merge!(
   }
 )
 
-# Bootstrap aws CLI auth before any cookbook that uses require_external_auth.
-# aws-credentials writes ~/.aws/config (login_session for sh1admn admin)
-# at compile time so `aws login --profile sh1admn --remote` works, then
-# uses sh1admn creds to fetch pve-bootstrap-ssm access keys from SSM and
-# write them to ~/.aws/credentials. Downstream cookbooks (cognee,
-# auto-mitamae-target, etc.) then find pve-bootstrap-ssm pre-configured.
-node.reverse_merge!(
-  aws_credentials: {
-    bootstrap_profile: "sh1admn",
-    profiles: {
-      "pve-bootstrap-ssm" => {
-        access_key_id_ssm:     "/home-monitor/iam/pve-bootstrap-ssm/access-key-id",
-        secret_access_key_ssm: "/home-monitor/iam/pve-bootstrap-ssm/secret-access-key",
-        region:                "ap-northeast-1",
-      },
-    },
-  }
-)
-include_cookbook "aws-credentials"
-
 include_cookbook "docker-engine"
 include_cookbook "cognee"
 include_role "lxc-core"
